ad83fd587dcfcc443c3ab7b9aef1cd155c19cc70
Coleman Strauss <colepat6@gmail.com>
2013-03-11 23:03:10 -0500

    Created a rails application and added status updadtes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myfitsa::Application.config.secret_token = 'e4d6d192e1b79b55028949d2e928389d8a2da2d60ab5c2ab7c3fc7f7791b832ab66731dc91434cebd5c9728f2571001c5c0e5527385fc34cf3954e0fc2ff8eca'
